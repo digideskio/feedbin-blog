252861ecb94e4587da2e7d85a8ad7d46bbfd1890
Ben Ubois <ben@benubois.com>
2013-12-05 03:06:58 -0800

    PuSH support.

---
+++ _plugins/push_publish.rb
@@ -0,0 +1,16 @@
+require 'net/http'
+
+module Jekyll
+  class PushPublish < Generator
+    safe true
+
+    def generate(site)
+      uri = URI('http://pubsubhubbub.superfeedr.com/')
+      Net::HTTP.post_form(uri,
+        'hub.mode' => "publish",
+        'hub.url' => 'http://blog.feedbin.me/atom.xml'
+      )
+    end
+
+  end
+end
